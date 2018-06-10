36dd488de4a589e1d817b4bc8d3d39001de802a0
Greg Turek <greg.m.turek@gmail.com>
2018-06-10 17:32:48 -0500

    Handle rotation and initial navigation

--- app/src/main/java/com/casalasglorias/MainActivity.java
+++ app/src/main/java/com/casalasglorias/MainActivity.java
@@ -62,6 +62,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+        navigationView.getMenu().performIdentifierAction(R.id.nav_home, 0);
     }
 
     @Override
